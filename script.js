dfaa128e16ab3334233719fe69c484a87481f87b
Steve1192 <stevesantner@gmail.com>
2022-11-15 13:19:41 -0500

    successfully added event listener!

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Mark Sanchez did his best";
 document.querySelector("body").appendChild(h2);
+addEventListener("click", function() {this.alert("Pats suck!");});
